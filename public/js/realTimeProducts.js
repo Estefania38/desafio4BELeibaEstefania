386691bbbfa46c1bc1f013351ec5afccb0a8430b
Estefania38 <leibaestefania@gmail.com>
2023-08-14 02:19:16 -0300

    completando datos

--- public/js/realTimeProducts.js
+++ public/js/realTimeProducts.js
@@ -1,36 +1,96 @@
-const socketClient = io();
+const socketClient = io()
 
-socketClient.on("enviodeproductos", (obj) => {
-  const list = document.querySelector('#list-products')
-  console.log(obj);
-    list.innerHTML = ''
-  obj.forEach((product) => {
-    list.innerHTML += `
-            <article class="container">
-          <div class="card">
-            <div class="imgBx">
-              <img src="${product.thumbnail}" width="90px" />
-            </div>
-            <div class="contentBx">
-              <h2>${product.title}</h2>
-              <div class="size">
-                <h3>${product.description}</h3>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+
+socketClient.on("enviodeproducts", (obj) => {
+  updateProductList(obj)
+})
+
+
+function updateProductList(products) {
+  let div = document.getElementById("contenedor");
+  let productos = "";
+
+  products.forEach((product) => {
+    productos += `
+          
+      <div class="card">
+      <div class="basicInfo">
+          <div class="title">
+              <div class="category">Nike</div>
+              <div class="name">${product.title}</div>
+              <div class="info">${product.description}</div>
+          </div>
+          <div class="images">
+              <div class="img">
+                  <div class="item">
+                      <img src="${product.thumbnail}">
+                  </div>
+                
               </div>
-              <div class="color">
-                <h3>${product.price}</h3>
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-              <a href="#">Buy Now</a>
-            </div>
-          </div>      
-        </article>          
-            `;
-      });
+          </div>
+          <div class="colors">
+              <label for="green">
+                  <div class="name">ss</div>
+                  <div class="ellipse" style="background:#CADB6E"></div>
+              </label>
+
+              <label for="black">
+                  <div class="name">Black</div>
+                  <div class="ellipse" style="background:#2B2B2B"></div>
+              </label>
+          </div>
+          <div class="addCard">
+              <i class="fa-solid fa-basket-shopping"></i>
+          </div>
+      </div>
+      <div class="mores">
+          <div class="stars">
+              <i class="fa-regular fa-star text-yellow"></i>
+              <i class="fa-regular fa-star text-yellow"></i>
+              <i class="fa-regular fa-star text-yellow"></i>
+              <i class="fa-regular fa-star text-yellow"></i>
+              <i class="fa-regular fa-star"></i>
+          </div>
+          <div class="price">${product.price}</div>
+      </div>
+
+  </div>
+          `;
+  });
+
+  div.innerHTML = productos;
+}
+
+
+let form = document.getElementById("formProduct");
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  let title = form.elements.title.value;
+  let description = form.elements.description.value;
+  let stock = form.elements.stock.value;
+  let thumbnail = form.elements.thumbnail.value;
+  let category = form.elements.category.value;
+  let price = form.elements.price.value;
+  let code = form.elements.code.value;
+
+  socketClient.emit("addProduct", {
+    title,
+    description,
+    stock,
+    thumbnail,
+    category,
+    price,
+    code,
+  });
+
+  form.reset();
 });
 
+document.getElementById("delete-btn").addEventListener("click", function () {
+  const deleteidinput = document.getElementById("id-prod");
+  const deleteid = deleteidinput.value;
+  console.log(deleteid)
+  socketClient.emit("deleteProduct", deleteid);
+  deleteidinput.value = "";
+});
